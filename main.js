612ece600b538398465f90927dd3c92493b25105
sowluss <sowluss@gmail.com>
2018-11-06 22:16:00 +0200

    used alternative forEach method to output numbers and sybmols to the screen, eval() function still shows null

--- main.js
+++ main.js
@@ -3,21 +3,31 @@ const screen = document.querySelector('.screen');
 const equal = document.querySelector('.btn-equal');
 const clear = document.querySelector('.btn-clear');
 
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function () {
-    let number = btns[i].getAttribute('data-num');
-    // adds the output to the screen
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', function () {
+//     let number = btns[i].getAttribute('data-num');
+
+    //* adds output to the screen
+//     screen.value += number;
+//   });
+// };
+
+// * forEach method
+
+btns.forEach(function(btn){
+  btn.addEventListener('click', function(){
+    let number = btn.getAttribute('data-num');
     screen.value += number;
   });
-};
+});
 
+// ? there is an error in let result = eval(screen.value); line giving the result as null
 equal.addEventListener('click', function () {
   if (screen.value === '') {
-    screen.value = 'Input is empty';
+    screen.value = `Input is empty`;
   } else {
     let result = eval(screen.value);
     screen.value = result;
-    
   }
 })
 
